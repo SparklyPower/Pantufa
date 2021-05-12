eedf14907452eba3c52d3ecc3591744fdcb792da
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-05-11 22:06:21 -0300

    Make it compilable

--- src/main/kotlin/net/perfectdreams/pantufa/PantufaLauncher.kt
+++ src/main/kotlin/net/perfectdreams/pantufa/PantufaLauncher.kt
@@ -1,40 +1,21 @@
 package net.perfectdreams.pantufa
 
-import net.perfectdreams.pantufa.utils.PantufaConfig
+import com.typesafe.config.ConfigFactory
+import kotlinx.serialization.hocon.Hocon
+import kotlinx.serialization.hocon.decodeFromConfig
 import java.io.File
 
 object PantufaLauncher {
 	@JvmStatic
 	fun main(args: Array<String>) {
-		val config = File("./pantufa.txt").readLines()
-
-		val token = config[0]
-		val postgreSqlIp = config[1]
-		val postgreSqlPort = config[2]
-		val postgreSqlUsername = config[3]
-		val postgreSqlPassword = config[4]
-
-		val postgreSqlCraftConomyIp = config[5]
-		val postgreSqlCraftConomyPort = config[6]
-		val postgreSqlCraftConomyUsername = config[7]
-		val postgreSqlCraftConomyPassword = config[8]
-
 		val pantufa = PantufaBot(
-				PantufaConfig(
-						token,
-						PantufaConfig.PostgreSqlConfig(
-								postgreSqlIp,
-								postgreSqlPort.toInt(),
-								postgreSqlUsername,
-								postgreSqlPassword
-						),
-						PantufaConfig.PostgreSqlConfig(
-								postgreSqlCraftConomyIp,
-								postgreSqlCraftConomyPort.toInt(),
-								postgreSqlCraftConomyUsername,
-								postgreSqlCraftConomyPassword
-						)
+			Hocon.decodeFromConfig(
+				ConfigFactory.parseFile(
+					File(
+						"./pantufa.conf"
+					)
 				)
+			)
 		)
 
 		pantufa.start()
