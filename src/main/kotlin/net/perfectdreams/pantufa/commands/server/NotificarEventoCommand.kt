0c10aef9399b9eb69b85d8a32eac4717b3591ad9
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-09-28 21:43:22 -0300

    Add enquete choice

--- src/main/kotlin/net/perfectdreams/pantufa/commands/server/NotificarEventoCommand.kt
+++ src/main/kotlin/net/perfectdreams/pantufa/commands/server/NotificarEventoCommand.kt
@@ -14,14 +14,15 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object NotificarEventoCommand {
 	val eventRoles = mapOf(
-			"quiz" to 555184377504464898L,
-			"dropparty" to 539979402143072267L,
-			"minarecheada" to 538805118384996392L,
-			"labirinto" to 539471142772146176L,
-			"corrida" to 477979984275701760L,
-			"fight" to 798697267312853002L,
-			"especial" to 798697126573506571L,
-			"mcmmo2x" to 798696876052185119L
+		"quiz" to 555184377504464898L,
+		"dropparty" to 539979402143072267L,
+		"minarecheada" to 538805118384996392L,
+		"labirinto" to 539471142772146176L,
+		"corrida" to 477979984275701760L,
+		"fight" to 798697267312853002L,
+		"especial" to 798697126573506571L,
+		"mcmmo2x" to 798696876052185119L,
+		"enquete" to 1157110842815418408L
 	)
 
 	fun create(pantufa: PantufaBot) = command(pantufa, "NotificarEventoCommand", listOf("notificar evento")) {
@@ -30,9 +31,9 @@ object NotificarEventoCommand {
 			val guild = this.message.guild
 			if (guild.idLong != 320248230917046282L) {
 				reply(
-						PantufaReply(
-								"Você só pode usar o comando no servidor do SparklyPower! https://discord.gg/sparklypower"
-						)
+					PantufaReply(
+						"Você só pode usar o comando no servidor do SparklyPower! https://discord.gg/sparklypower"
+					)
 				)
 				return@executes
 			}
@@ -41,9 +42,9 @@ object NotificarEventoCommand {
 
 			if (eventName == null) {
 				reply(
-						PantufaReply(
-								"Escolha um evento para eu te notificar! ${eventRoles.keys.joinToString(", ") { "`$it`" }}"
-						)
+					PantufaReply(
+						"Escolha um evento para eu te notificar! ${eventRoles.keys.joinToString(", ") { "`$it`" }}"
+					)
 				)
 				return@executes
 			} else {
@@ -51,18 +52,18 @@ object NotificarEventoCommand {
 
 				if (eventRoleId == null) {
 					reply(
-							PantufaReply(
-									"Evento inválido! Eventos suportados: ${eventRoles.keys.joinToString(", ") { "`$it`" }}"
-							)
+						PantufaReply(
+							"Evento inválido! Eventos suportados: ${eventRoles.keys.joinToString(", ") { "`$it`" }}"
+						)
 					)
 					return@executes
 				}
 
 				val role = guild.getRoleById(eventRoleId) ?: run {
 					reply(
-							PantufaReply(
-									"Parece que o cargo desse evento não existe mais... Avise para a equipe!"
-							)
+						PantufaReply(
+							"Parece que o cargo desse evento não existe mais... Avise para a equipe!"
+						)
 					)
 					return@executes
 				}
@@ -70,17 +71,17 @@ object NotificarEventoCommand {
 				if (member.roles.contains(role)) {
 					guild.removeRoleFromMember(member, role).await()
 					reply(
-							PantufaReply(
-									"Cargo removido com sucesso! Cansou do evento para remover a notificação dele?"
-							)
+						PantufaReply(
+							"Cargo removido com sucesso! Cansou do evento para remover a notificação dele?"
+						)
 					)
 					return@executes
 				} else {
 					guild.addRoleToMember(member, role).await()
 					reply(
-							PantufaReply(
-									"Cargo adicionado com sucesso! Quero ver você sempre no evento, hein?"
-							)
+						PantufaReply(
+							"Cargo adicionado com sucesso! Quero ver você sempre no evento, hein?"
+						)
 					)
 					return@executes
 				}
