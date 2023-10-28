0067a13730705feda450a34acf69b8c7a21da3a8
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-10-28 11:54:36 -0300

    Change to Loritta's DeviousJDA

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     runtimeOnly("ch.qos.logback:logback-classic:1.4.11")
     implementation("io.github.microutils:kotlin-logging:2.1.23")
 
-    implementation("net.dv8tion:JDA:5.0.0-beta.3")
+    implementation("com.github.LorittaBot:DeviousJDA:6644ac5cb9")
     implementation("com.github.MinnDevelopment:jda-ktx:9370cb13cc")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
